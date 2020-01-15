cb9f9991f4e02007d66c13ade364db6631e4a33d
firmino <firminodefaria@gmail.com>
2020-01-14 22:52:54 -0300

    small change

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.get('/', function(req, res) {
       `
       <html>
         <body>
-          <h1> EKANNNSSSSS </h1>
+         
           <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/023.png">
         </body>
       </html>`)
